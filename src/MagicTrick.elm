74a9db4b7f552820db83ac39b1ca28ad96bd8249
fonzerelly <christian.hoerauf@gmail.com>
2020-06-14 22:15:46 +0200

    implements mergeGame

--- src/MagicTrick.elm
+++ src/MagicTrick.elm
@@ -1,7 +1,5 @@
-module MagicTrick exposing (handOut, Game)
+module MagicTrick exposing (handOut, Game, mergeGame, UserSelection(..))
 import Deck exposing (..)
-import Main exposing (Msg(..))
-import Deck exposing (ShuffledDeck(..))
 import Cards exposing (Card(..))
 import List
 
@@ -12,49 +10,9 @@ type alias Game = { left:  ShuffledDeck
 
 emptyDeck: ShuffledDeck
 emptyDeck = newDeck []
--- implement appendLeft appendCenter and appendRight
--- pass that function to handOutRecursivly
--- 
--- appendLeft: Card -> Game -> Game
--- appendLeft card game = { game | left = appendCard card (.left game)}
 
--- appendCenter: Card -> Game -> Game
--- appendCenter card game = { game | center = appendCard card (.center game)}
-
--- appendRight: Card -> Game -> Game
--- appendRight card game = { game | left = appendCard card (.left game)}
-
--- type GameDeck = Left | Center | Right
-
--- handOutRecursivly: (Game, ShuffledDeck, GameDeck)-> (Game, ShuffledDeck, GameDeck)
--- handOutRecursivly gameState =
---     let 
---         (game, deck, gamedeck) = gameState
---         (nextCard, restDeck) = draw deck
---     in
---         case nextCard of
---            Back -> (game, restDeck, Left)
---            _ -> 
---                 case gamedeck of
---                     Left -> (appendLeft nextCard game, restDeck, Center)
---                     Center -> (appendCenter nextCard game, restDeck, Right)
---                     Right -> (appendRight nextCard game, restDeck, Left)
-
--- handOut : ShuffledDeck -> Game
--- handOut deck = 
---     let
---         (game, _, _) = handOutRecursivly 
---             ( { left = emptyDeck
---               , center = emptyDeck
---               , right = emptyDeck 
---               }
---             , deck
---             , Left
---             )
---     in
---         game
 cardOfTuple: (Int, Card) -> Card
-cardOfTuple (index, card) = card
+cardOfTuple (_, card) = card
 
 isNthOf: Int -> (Int, a) -> Bool
 isNthOf n (index, _) = n == modBy 3 index
@@ -70,3 +28,16 @@ handOut deck =
     , center = newDeck (List.map cardOfTuple (List.filter (isNthOf <| 1) indexedCards))
     , right = newDeck (List.map cardOfTuple (List.filter (isNthOf <| 2) indexedCards))
     }
+
+type UserSelection = UserTookLeft | UserTookCenter | UserTookRight
+mergeGame : UserSelection -> Game -> ShuffledDeck
+mergeGame selection game = 
+    let
+        listOfLeft = .left >> getCards
+        listOfCenter = .center >> getCards
+        listOfRight = .right >> getCards
+    in
+        case selection of
+            UserTookLeft -> newDeck (listOfCenter game ++ listOfLeft game ++ listOfRight game)
+            UserTookRight -> newDeck (listOfLeft game ++ listOfRight game ++ listOfCenter game)
+            UserTookCenter -> newDeck (listOfLeft game ++ listOfCenter game ++ listOfRight game)
--- tests/MagicTrickTests.elm
+++ tests/MagicTrickTests.elm
@@ -6,11 +6,10 @@ import Expect
 import Cards exposing (..)
 import Deck exposing (..)
 
-import MagicTrick exposing (handOut, Game)
+import MagicTrick exposing (handOut, Game, mergeGame, UserSelection(..))
 import List
+import Cards exposing (Face(..), Suit(..))
 import Deck exposing (ShuffledDeck(..))
-import Cards exposing (Face(..))
-import Cards exposing (Suit(..))
 
 deckSize: ShuffledDeck -> Int
 deckSize = getCards >> List.length
@@ -24,20 +23,20 @@ centerSize = .center >> deckSize
 rightSize: Game -> Int
 rightSize = .center >> deckSize
 
+emptyDeck: ShuffledDeck
+emptyDeck = newDeck []
+
 all : Test
 all = 
     describe "MagicTrick"
         [ describe "handOut"
             [ test "should return a list of three decks" <|
                 \_ -> 
-                    let
-                        emptyDeck = Deck.take 0 Deck.fullDeck
-                    in
-                        handOut emptyDeck |> Expect.all 
-                            [ \result -> leftSize result |> Expect.equal 0
-                            , \result -> centerSize result |> Expect.equal 0
-                            , \result -> rightSize result |> Expect.equal 0
-                            ]
+                    handOut emptyDeck |> Expect.all 
+                        [ \result -> leftSize result |> Expect.equal 0
+                        , \result -> centerSize result |> Expect.equal 0
+                        , \result -> rightSize result |> Expect.equal 0
+                        ]
             , test "should split shuffledDeck up to three decks" <|
                 \_ ->
                     let
@@ -69,5 +68,91 @@ all =
                             , \result -> .center result |> Expect.equal (expectedCenter)
                             , \result -> .right result |> Expect.equal (expectedRight)
                             ]
+            ]
+        , describe "mergeGame"
+            [ test "should merge an empty game to a empty deck" <|
+                \_ ->
+                    let 
+                        emptyGame = 
+                            { left = emptyDeck
+                            , center = emptyDeck
+                            , right = emptyDeck
+                            }
+                    in
+                        mergeGame UserTookLeft emptyGame |> Expect.equal (newDeck [])
+
+            , test "should merge left deck into middle when user selects left" <|
+                \_ ->
+                    let
+                        game = { left = newDeck [Card Hearts Ace]
+                               , center = newDeck [Card Spades Ace]
+                               , right = newDeck [Card Clubs Ace]
+                               }
+                        expectedDeck = newDeck [ Card Spades Ace
+                                               , Card Hearts Ace
+                                               , Card Clubs Ace
+                                               ]
+                    in
+                        mergeGame UserTookLeft game |> Expect.equal expectedDeck
+           
+            , test "should merge right deck into middle when user selects r" <|
+                \_ ->
+                    let
+                        game = { left = newDeck [Card Hearts Ace]
+                               , center = newDeck [Card Spades Ace]
+                               , right = newDeck [Card Clubs Ace]
+                               }
+                        expectedDeck = newDeck [ Card Hearts Ace
+                                               , Card Clubs Ace
+                                               , Card Spades Ace
+                                               ]
+                    in
+                        mergeGame UserTookRight game |> Expect.equal expectedDeck
+           
+            , test "should merge center deck into middle when user selects center" <|
+                \_ ->
+                    let
+                        game = { left = newDeck [Card Hearts Ace]
+                               , center = newDeck [Card Spades Ace]
+                               , right = newDeck [Card Clubs Ace]
+                               }
+                        expectedDeck = newDeck [ Card Hearts Ace
+                                               , Card Spades Ace
+                                               , Card Clubs Ace
+                                               ]
+                    in
+                        mergeGame UserTookCenter game |> Expect.equal expectedDeck
+                                   
+            , test "should merge a deck of nine cards" <|
+                \_ ->
+                    let
+                        game = { left = newDeck 
+                                    [ Card Hearts Ace
+                                    , Card Hearts King
+                                    , Card Hearts Queen
+                                    ]
+                               , center = newDeck 
+                                    [ Card Spades Ace
+                                    , Card Spades King
+                                    , Card Spades Queen
+                                    ]
+                               , right = newDeck 
+                                    [ Card Clubs Ace
+                                    , Card Clubs King
+                                    , Card Clubs Queen
+                                    ]
+                               }
+                        expectedDeck = newDeck [ Card Spades Ace
+                                               , Card Spades King
+                                               , Card Spades Queen
+                                               , Card Hearts Ace
+                                               , Card Hearts King
+                                               , Card Hearts Queen
+                                               , Card Clubs Ace
+                                               , Card Clubs King
+                                               , Card Clubs Queen
+                                               ]
+                    in
+                        mergeGame UserTookLeft game |> Expect.equal expectedDeck
             ]
         ]
