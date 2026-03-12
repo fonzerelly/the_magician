a83bc39cdd8a8eb422e98eb3bcd8f59410028d5a
fonzerelly <christian.hoerauf@gmail.com>
2026-03-12 23:56:01 +0100

    Precents usage of elm-home

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ echo "Building..."
 rm -rf dist
 mkdir -p dist
 
-ELM_HOME="$(pwd)/elm-home" ./bin/elm make src/Main.elm --optimize --output=dist/elm.js
+./bin/elm make src/Main.elm --output=dist/Elm.js
 
 mkdir -p dist/src
 cp -r src/card-deck dist/src/card-deck
@@ -16,14 +16,14 @@ cat > dist/index.html <<'EOF'
 <!DOCTYPE html>
 <html lang="de">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>The Magician</title>
-    <style>body { margin: 0; }</style>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>The Magician</title>
+  <style>body { margin: 0; }</style>
 </head>
 <body>
-    <script src="elm.js"></script>
-    <script>Elm.Main.init();</script>
+  <script src="Elm.js"></script>
+  <script>Elm.Main.init();</script>
 </body>
 </html>
 EOF
